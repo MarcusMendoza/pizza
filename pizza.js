503637502c57cb128240416d1b808969d62d520c
MarcusMendoza <7mmendoza@stpatrick.org>
2016-11-18 13:54:29 -0600

    day 1

---
+++ pizza.js
@@ -0,0 +1,28 @@
+class pizza{
+  //type constructor below
+constructor(s,m,v){
+this.size=s;
+this.meatToppings[];
+this.veggieToppings[];
+}
+
+
+  //type instance functions below
+sizecost()
+if(this.size="s"){
+let price=7.99;
+}
+if(this.size="m"){
+let price=9.99;
+}
+if(this.size="l"){
+let price=12.99;
+}
+if(this.size="xl"){
+let price=15.99
+}
+
+  //type class functions below
+
+
+}
